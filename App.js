d90df679498fd546b7f3256ec99ea1805b4ff39f
John Martin <curmudgeon@rallydev.com>
2013-03-29 09:32:08 -0700

    first cut with only showing tasks for defects

---
+++ App.js
@@ -0,0 +1,40 @@
+var parentRenderer = function(value) {
+    return value._refObjectName;
+}
+
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items: [ { xtype: 'container', itemId: 'grid_box' }],
+    launch: function() {
+        this._getTasks();
+    },
+    _getTasks: function() {
+        var store = Ext.create('Rally.data.WsapiDataStore',{
+            model: 'Task',
+            autoLoad: true,
+            listeners: {
+                load: function(store,data,success){
+                    // remove items that aren't attached to defects
+                    Ext.Array.each( data, function(item){
+                        if ( item.get('WorkProduct')._type !== "Defect" ) {
+                            store.remove(item);
+                        }
+                    });
+                    this._showGrid(store);
+                },
+                scope: this
+            }
+        });
+    },
+    _showGrid: function(store) {
+        this.grid = Ext.create('Rally.ui.grid.Grid',{
+            store: store,
+            columnCfgs: [ 
+                { text: 'ID', dataIndex: 'FormattedID' }, 
+                {text: 'Name', dataIndex: 'Name', flex: 1},
+                {text: 'Parent', dataIndex: 'WorkProduct', renderer: parentRenderer } ]
+        });
+        this.down('#grid_box').add(this.grid);
+    }
+});
